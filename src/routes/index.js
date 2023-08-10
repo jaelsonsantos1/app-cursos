2e8ffd8506be43ece66b404cb3d184c6f1a82dd9
jaelsonsantos1 <jaelsons860@gmail.com>
2023-08-10 13:19:48 -0300

    Correção na importação das páginas Welcome e SignIn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Welcome from "../pages/welcome";
-import SignIn from "../pages/Signin";
+import Welcome from "../pages/Welcome";
+import SignIn from "../pages/SignIn";
 
 const Stack = createNativeStackNavigator();
 
