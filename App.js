f27097883d477dfaf40550419602e52b823eabef
jaelsonsantos1 <jaelsons860@gmail.com>
2023-08-10 10:49:15 -0300

    Configuração para navegação das telas

--- App.js
+++ App.js
@@ -1,63 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import './src/config/firebase'
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes';
 
 export default function App() {
-  const [estado, setEstado] = useState({
-    email: '',
-    senha: ''
-  })
-
-  const auth = getAuth()
-
   return (
-    <View style={styles.container}>
-      <TextInput placeholder='Email' value={estado.email} onChangeText={(value) => {
-        setEstado({...estado, email: value})
-      }} style={{borderWidth: 1, borderColor: '#c8c8c8', height: 40, width: 200, padding: 5, borderRadius: 15}}></TextInput>
-      <TextInput placeholder='Senha' value={estado.senha} onChangeText={(value) => {
-        setEstado({...estado, senha: value})
-      }} style={{borderWidth: 1, borderColor: '#c8c8c8', height: 40, width: 200, padding: 5, borderRadius: 15}}></TextInput>
-
-      <Button title='Create user' onPress={async () => {
-        try {
-          await createUserWithEmailAndPassword(auth, estado.email, estado.senha);
-          console.log("User created.");
-        } catch(error) {
-          console.log(error);
-        }
-
-      }}/>
-      <Button title='Login' onPress={async () => {
-        try {
-          await signInWithEmailAndPassword(auth, estado.email, estado.senha);
-          console.log('Loggin Sucessfull!');
-        } catch(error) {
-          console.log(error);
-        }
-      }}/>
-
-      <Button title='Exit' onPress={() => {
-        try {
-          signOut(auth);
-          console.log('Saiu!');
-        } catch(error) {
-          console.log(error);
-        }
-      }}/>
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor="#111B32" barStyle="light-content"
+      />
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}); 
