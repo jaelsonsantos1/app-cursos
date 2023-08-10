059c13256bbdb90c0fd47b8ff549cf644d9ecd2a
jaelsonsantos1 <jaelsons860@gmail.com>
2023-08-10 11:01:08 -0300

    Criação das paginas SignIn e Welcome

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SignIn() {
+    return (
+        <View>
+            <Text>
+                Bem vindo a tela de Login
+            </Text>
+        </View>
+    )
+}
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,15 @@
+import {
+    View,
+    Text
+} from 'react-native';
+
+import styles from './styles';
+
+export default function Welcome() {
+    return (
+        <View>
+            <Text>Bem vindo!</Text>
+        </View>
+    )
+
+}
