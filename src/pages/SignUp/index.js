5148241fbfaa13569a9e3f955cd5d96138037c16
jaelsonsantos1 <jaelsons860@gmail.com>
2023-08-11 12:21:05 -0300

    Adicionando pagina de cadastro

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,13 @@
+import {
+    View, Text
+} from 'react-native';
+
+export default function SingUp() {
+    return (
+        <View>
+            <Text>
+                Bem vindo a tela de cadastro
+            </Text>
+        </View>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Welcome from "../pages/Welcome";
 import SignIn from "../pages/SignIn";
+import SingUp from "../pages/SignUp";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,13 @@ export default function Routes() {
             <Stack.Screen
                 name="SignIn"
                 component={SignIn}
+                options={{
+                    HeaderShown: false
+                }}
+            />
+            <Stack.Screen
+                name="SignUp"
+                component={SingUp}
                 options={{
                     HeaderShown: false
                 }}
