12a983751814f2c237d464531830121e1c0b5b30
jaelsonsantos1 <jaelsons860@gmail.com>
2023-08-11 12:22:43 -0300

    Pagina welcome finalizada

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,15 +1,67 @@
-import {
+import { 
     View,
-    Text
+    Text,
+    Image,
+    TouchableOpacity
 } from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
 export default function Welcome() {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>Bem vindo!</Text>
+        <View style={styles.container}>
+
+            {/* Forma azul superior */}
+            <Animatable.View
+                animation="fadeInDown"
+                style={styles.containerLogo}
+            >
+
+                {/* Base circular para imagem logo */}
+                <Animatable.View
+                    delay={600}
+                    animation={"flipInY"}
+                    style={styles.shapeLogo}
+                >
+                    {/* Imagem logo */}
+                    <Image
+                        source={require('../../assets/img/icone-logo.png')}
+                        style={{width:'100%'}}
+                    />
+                </Animatable.View>
+            </Animatable.View>
+
+            {/* Área para os textos e o botão */}
+            <View style={styles.containerForm}>
+
+                {/* Titulo */}
+                <Text style={styles.title}>
+                    CursoX
+                </Text>
+
+                {/* Frase abaixo do título */}
+                <Text style={styles.Text}>
+                    Invista em si mesmo, torne-se uma versão melhor de você!
+                </Text>
+
+                {/* Botão para entrar no aplicativo */}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        navigation.navigate('SignIn');
+                    }}
+                >
+                    {/* Texto do botão */}
+                    <Text style={styles.textButton}>
+                        Comece agora!
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
-
 }
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -1,7 +1,56 @@
 import { StyleSheet } from "react-native";
 
-export default function styles() {
-    return StyleSheet.create({
-        
-    })
-} 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
+    containerLogo: {
+        backgroundColor: '#111B32',
+        width: '100%',
+        flex: 1,
+        borderBottomLeftRadius: 100,
+        borderBottomRightRadius: 100,
+    },
+    shapeLogo: {
+        position: 'absolute',
+        bottom: -66.2,
+        left: '35%',
+        width: 133,
+        height: 133,
+        backgroundColor: '#F4F4F4',
+        borderRadius: 133,
+        justifyContent: 'center',
+        alignItems: 'center' 
+    },
+    containerForm: {
+        flex: 2,
+        width: '100%',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 40,
+        fontWeight:'bold',
+        marginTop: '30%',
+    },
+    Text: {
+        fontSize: 12,
+        marginTop: 35,
+    },
+    button: {
+        position: 'absolute',
+        bottom: '15%',
+        backgroundColor: '#A5FC83',
+        height: 45,
+        width: '60%',
+        borderRadius: 39,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton: {
+        fontSize: 12,
+        color: '#000',
+    }
+})
+
+export default styles;
